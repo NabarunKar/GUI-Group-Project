305b5a6aa151254b80c00fc9d784c16ff16648bc
archiportal <royarchisman17@gmail.com>
2021-03-02 21:54:38 +0530

    ui fixed

--- main.py
+++ main.py
@@ -145,18 +145,18 @@ bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 logo_image = Image.open(r"images\logo.png")
 logo = ImageTk.PhotoImage(logo_image)
 logo_label = Label(image=logo)
-logo_label.place(x=80, y=100, width=280, height=380)
+logo_label.place(x=140, y=100, width=290, height=480)
 
 #  REGISTRATION FRAME #
 
 # we are making the frame in the root window
 frame1 = Frame(root, bg="white")
-frame1.place(x=360, y=100, width=730, height=380)
+frame1.place(x=430, y=100, width=730, height=480)
 
 # title
 title = Label(frame1, text="PLEASE ENTER YOUR DETAILS HERE", font=(
-    "times new roman", 15, "bold"), bg="white", fg="green")
-title.place(x=50, y=5)
+    "times new roman", 18, "bold"), bg="white", fg="brown")
+title.place(x=70, y=5)
 
 # STUDENT NAME #
 student_Label = Label(frame1, text="NAME  ", font=(
@@ -170,142 +170,142 @@ student_name.place(x=150, y=50)
 #PARENT'S NAME #
 fname_label = Label(frame1, text="FATHER'S NAME  ", font=(
     "verdana", 10, "bold"), bg="white", fg="black")
-fname_label.place(x=10, y=70)
+fname_label.place(x=10, y=80)
 
 fname = Entry(frame1, font=("verdana", 10),
               bg="lightgray", textvariable=fatherName)
-fname.place(x=150, y=70)
+fname.place(x=150, y=80)
 
 mname_label = Label(frame1, text="MOTHER'S NAME  ", font=(
     "verdana", 10, "bold"), bg="white", fg="black")
-mname_label.place(x=320, y=70)
+mname_label.place(x=360, y=80)
 
 mname = Entry(frame1, font=("verdana", 10),
               bg="lightgray", textvariable=motherName)
-mname.place(x=500, y=70)
+mname.place(x=500, y=80)
 
 
 # MAIL ID #
 mail_label = Label(frame1, text="EMAIL ID  ", font=(
     "verdana", 10, "bold"), bg="white", fg="black")
-mail_label.place(x=10, y=90)
+mail_label.place(x=10, y=110)
 
 mail = Entry(frame1, font=("verdana", 10),
              bg="lightgray", textvariable=email, width=30)
-mail.place(x=150, y=90)
+mail.place(x=150, y=110)
 
 
 # SEX #
 sex_label = Label(frame1, text="SEX  ", font=(
     "verdana", 10, "bold"), bg="white", fg="black")
-sex_label.place(x=10, y=110)
+sex_label.place(x=10, y=140)
 
 # RADIOBUTTON
 Radiobutton(frame1, text="MALE", value=1,
-            variable=gender).place(x=150, y=110)
+            variable=gender).place(x=150, y=140)
 
 Radiobutton(frame1, text="FEMALE", value=2,
-            variable=gender).place(x=230, y=110)
+            variable=gender).place(x=213, y=140)
 
 Radiobutton(frame1, text="OTHER", value=3,
-            variable=gender).place(x=310, y=110)
+            variable=gender).place(x=288, y=140)
 
 
 # CONTACT NUMBER #
 phone_label = Label(frame1, text="PHONE NUMBER  ", font=(
     "verdana", 10, "bold"), bg="white", fg="black")
-phone_label.place(x=10, y=130)
+phone_label.place(x=10, y=170)
 
 phone = Entry(frame1, font=("verdana", 10),
               bg="lightgray", textvariable=phoneNumber)
-phone.place(x=150, y=130)
+phone.place(x=150, y=170)
 
 # ADDRESS #
 present_address_label = Label(frame1, text="PRESENT ADDRESS  ", font=(
     "verdana", 10, "bold"), bg="white", fg="black")
-present_address_label.place(x=10, y=150)
+present_address_label.place(x=10, y=200)
 
 present_address = Entry(frame1, font=("verdana", 10),
                         bg="lightgray", textvariable=presentAddress)
-present_address.place(x=150, y=150)
+present_address.place(x=150, y=200)
 
 permanent_address_label = Label(frame1, text="PERMANENT ADDRESS  ", font=(
     "verdana", 10, "bold"), bg="white", fg="black")
-permanent_address_label.place(x=320, y=150)
+permanent_address_label.place(x=320, y=200)
 
 permanent_address = Entry(frame1, font=("verdana", 10),
                           bg="lightgray", textvariable=permanentAddress)
-permanent_address.place(x=500, y=150)
+permanent_address.place(x=500, y=200)
 
 
 # CLASS 10 #
 ten_label = Label(frame1, text="X BOARD MARKS", font=(
     "verdana", 10, "bold"), bg="white", fg="black")
-ten_label.place(x=10, y=170)
+ten_label.place(x=10, y=230)
 
 ten = Entry(frame1, font=("verdana", 10),
             bg="lightgray", textvariable=tenMarks)
-ten.place(x=150, y=170)
+ten.place(x=150, y=230)
 
 
 # CLASS 12 #
 twelve_label = Label(frame1, text="XII BOARD MARKS", font=(
     "verdana", 10, "bold"), bg="white", fg="black")
-twelve_label.place(x=320, y=170)
+twelve_label.place(x=350, y=230)
 
 twelve = Entry(frame1, font=("verdana", 10),
                bg="lightgray", textvariable=twelveMarks)
-twelve.place(x=500, y=170)
+twelve.place(x=500, y=230)
 
 
 # BOARD NAME #
 board_label = Label(frame1, text="BOARD", font=(
     "verdana", 10, "bold"), bg="white", fg="black")
-board_label.place(x=10, y=190)
+board_label.place(x=10, y=260)
 
 cmb_board = ttk.Combobox(frame1, font=(
     "verdane", 10, "bold"), values=("ISC", "STATE BOARD", "CBSE", "OTHER"))
-cmb_board.place(x=150, y=190)
+cmb_board.place(x=150, y=260)
 
 
 # PCM MARKS #
 phy_label = Label(frame1, text="MARKS IN PHY", font=(
     "verdana", 10, "bold"), bg="white", fg="black")
-phy_label.place(x=10, y=210)
+phy_label.place(x=10, y=290)
 
 phy = Entry(frame1, font=("verdana", 10),
             bg="lightgray", textvariable=phyMarks)
-phy.place(x=150, y=210)
+phy.place(x=150, y=290)
 
 chem_label = Label(frame1, text="MARKS IN CHEM", font=(
     "verdana", 10, "bold"), bg="white", fg="black")
-chem_label.place(x=10, y=230)
+chem_label.place(x=10, y=320)
 
 chem = Entry(frame1, font=("verdana", 10),
              bg="lightgray", textvariable=chemMarks)
-chem.place(x=150, y=230)
+chem.place(x=150, y=320)
 
 math_label = Label(frame1, text="MARKS IN MATH", font=(
     "verdana", 10, "bold"), bg="white", fg="black")
-math_label.place(x=10, y=250)
+math_label.place(x=10, y=350)
 
 math = Entry(frame1, font=("verdana", 10),
              bg="lightgray", textvariable=mathMarks)
-math.place(x=150, y=250)
+math.place(x=150, y=350)
 
 # checkbox
 chk_1 = Checkbutton(frame1,
                     text="I do hereby declare that all the information given above is true to the best of my knowledge and belief.",
                     bg="LightBlue1", activebackground="#80c1ff", font=("bold", 11), bd=15, variable=chk_var)
-chk_1.place(x=10, y=280)
+chk_1.place(x=10, y=380)
 
 # SUBMIT BUTTON
-Button(frame1, font=("verdana", 12), text="Submit",
-       command=show).place(x=260, y=340)
+Button(frame1, font=("verdana", 12), text="Submit",bg="#15A01F", activebackground = "#75F87E",
+       command=show).place(x=260, y=440)
 
 # RESET BUTTON
-Button(frame1, font=("verdana", 12), text="Reset",
-       command=removeData).place(x=360, y=340)
+Button(frame1, font=("verdana", 12), text="Reset",bg="#D74627", activebackground = "#F96259",
+       command=removeData).place(x=360, y=440)
 
 
 root.mainloop()
